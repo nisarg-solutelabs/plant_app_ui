a182987ba07596dd3d8c019a335256357edb1798
Nisarg Ratani <nisarg.r@soultelabs.com>
2021-04-27 09:44:52 +0530

    plant app ui

---
+++ lib/screens/home/components/body.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:plant_ui/constants.dart';
+
+import 'featurred_plants.dart';
+import 'header_with_seachbox.dart';
+import 'recomend_plants.dart';
+import 'title_with_more_bbtn.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          HeaderWithSearchBox(size: size),
+          TitleWithMoreBtn(title: "Recomended", press: () {}),
+          RecomendsPlants(),
+          TitleWithMoreBtn(title: "Featured Plants", press: () {}),
+          FeaturedPlants(),
+          SizedBox(height: kDefaultPadding),
+        ],
+      ),
+    );
+  }
+}
